bb51b4dece977e31adb2ebfb1011b7a1fb2da38c
dubisdev <dubisdev@gmail.com>
2023-03-30 21:49:40 +0200

    feat: add tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,61 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{AppHandle, Manager};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 use tauri_plugin_autostart::MacosLauncher;
-use tauri::Manager;
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  args: Vec<String>,
-  cwd: String,
+    args: Vec<String>,
+    cwd: String,
 }
 
 fn main() {
+    let tray = create_tray_menu();
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![""])))
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![""]),
+        ))
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-        app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
         }))
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => toggle_window(app),
+
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => std::process::exit(0),
+                "toggle" => toggle_window(app),
+                _ => {}
+            },
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn create_tray_menu() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("toggle".to_string(), "Toggle App");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(hide)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn toggle_window(app: &AppHandle) {
+    let window = app.get_window("main").unwrap();
+    if window.is_visible().unwrap() {
+        window.hide().unwrap();
+        return;
+    } else {
+        window.show().unwrap();
+        return;
+    }
 }
